78eea40d2f114d7f7496d9fe5fbb0fc7e58e41fa
juihee sharma <jsharma@box.com>
2015-10-28 09:45:17 -0700

    Product Catalog : Adding Home Page

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,25 +1,53 @@
 class CatalogController < SecuredController
-
-  helper_method :get_thumbnail
-
   # main catalog controller
-  def show
-
-    # get user client obj and file ID
+  def home
+    # get user client to make API calls
     client = user_client
-    @myFolder = Rails.cache.fetch("/folder/#{session[:box_id]}/my_folder", :expires_in => 10.minutes) do
-      client.folder_from_path('Product Catalog')
+    session[:current_page] = "catalog"
+
+    # Get root Product Catalog Folder from cache
+    @product_catalog = Rails.cache.fetch("/product_catalog_folder/#{ENV['PRODUCT_CATALOG_FOLDER']}", :expires_in => 20.minutes) do
+      client.folder_from_id(ENV['PRODUCT_CATALOG_FOLDER'], fields: [:id, :name, :size])
+
+      @file1 = Rails.cache.fetch("/product_catalog_folder/file/#{ENV['PRODUCT_CATALOG_FILE1']}", :expires_in => 20.minutes) do
+        client.file_from_id(ENV['PRODUCT_CATALOG_FILE1'], fields: [:id])
+      end
+
+      puts @file1
     end
 
-    @files = client.folder_items(@myFolder, fields: [:name, :id]).files
+    # get file id's from the product_catalog_folder
+    #
+    # @files = client.folder_items(@myFolder, fields: [:name, :id]).files
+    #
+
+    # @fileId = '41372508334'
+    # session[:fileId] = @fileId
+    #
+    # @file = client.file_from_id(@fileId)
+ end
+
+  def show
+    # get user client obj and file ID
+    client = user_client
+      puts "BEFORE Prod Catalog"
+    session[:current_page] = "catalog"
 
 
-    @fileId = '41372508334'
-    session[:fileId] = @fileId
 
-    @file = client.file_from_id(@fileId)
+    @product_catalog = Rails.cache.fetch("/product_catalog_folder/#{ENV['PRODUCT_CATALOG_FOLDER']}", :expires_in => 20.minutes) do
+        puts "AFTER Prod Catalog"
+      client.folder_from_id(ENV['PRODUCT_CATALOG_FOLDER'], fields: [:id, :name, :size])
+    end
+    #
+    # @files = client.folder_items(@myFolder, fields: [:name, :id]).files
+    #
 
-  end
+    # @fileId = '41372508334'
+    # session[:fileId] = @fileId
+    #
+    # @file = client.file_from_id(@fileId)
+ end
 
   def thumbnail
 
@@ -29,11 +57,6 @@ class CatalogController < SecuredController
     end
 
     send_data image, :type => 'image/png', :disposition => 'inline'
-  end
-  
-  # preview file
-  def preview
-    embed_url = user_client.embed_url(params[:id])
   end
 
   private
