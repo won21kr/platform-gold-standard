972757f3b93bbdc3c4d83e2523170a5b6675713a
Platform <mattmitchell650@gmail.com>
2015-12-07 12:53:53 -0800

    text change

--- app/controllers/medical_credentialing_controller.rb
+++ app/controllers/medical_credentialing_controller.rb
@@ -56,7 +56,7 @@ class MedicalCredentialingController < SecuredController
           session[:progress] = 3
           session[:med_task_status] = 0
           @medFiles = client.folder_items(@medFolder, fields: [:name, :id, :created_at, :modified_at]).files
-          @message = "Medical credentialingfolder request approved! Process complete."
+          @message = "Medical credentialing request approved! Process complete."
         else
           puts "Error: Something went wrong..."
       end
