73da2cf523df781ea506f479bf061bef987c7765
Platform <mattmitchell650@gmail.com>
2016-03-09 09:52:51 -0800

    working so far

--- app/controllers/media_content_controller.rb
+++ app/controllers/media_content_controller.rb
@@ -21,26 +21,39 @@ class MediaContentController < SecuredController
     @nbcItems = Rails.cache.fetch("/folder/#{session[:box_id]}/nbc_items", :expires_in => 10.minutes) do
       client.folder_items(@nbc, fields: [:id, :name, :description])
     end
+
     @nbcItems.each do |n|
       class << n
-        attr_accessor :image_id, :video_id
+        attr_accessor :imageId, :videoId
       end
       showFolder = client.folder_items(n, fields: [:id, :name])
       imageFile = showFolder.find {|s| s.name.split('.').last == 'jpg' }
       trailerFile = showFolder.find {|s| s.name.split('.').last == 'mp4' }
-      ap imageFile
-      ap trailerFile
 
+      n.imageId = imageFile.id
+      n.videoId = trailerFile.id
+    end
+
+
+    @usa = @networks.find {|s| s.name == 'USA Networks' }
+    @usaItems = Rails.cache.fetch("/folder/#{session[:box_id]}/usa_items", :expires_in => 10.minutes) do
+      client.folder_items(@usa, fields: [:id, :name, :description])
+    end
+    @usaItems.each do |n|
+      class << n
+        attr_accessor :imageId, :videoId
+      end
+      showFolder = client.folder_items(n, fields: [:id, :name])
+      imageFile = showFolder.find {|s| s.name.split('.').last == 'jpg' }
+      trailerFile = showFolder.find {|s| s.name.split('.').last == 'mp4' }
+
+      n.imageId = imageFile.id
+      n.videoId = trailerFile.id
     end
 
 
 
-
-
-
-
     @cnbc = @networks.find {|s| s.name == 'CNBC' }
-    @usa = @networks.find {|s| s.name == 'USA' }
     @e = @networks.find {|s| s.name == 'E!' }
 
 
