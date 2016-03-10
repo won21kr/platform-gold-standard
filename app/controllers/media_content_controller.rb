096e36cc604efd5c0da55d3dcfbc166ce0db35a3
Platform <mattmitchell650@gmail.com>
2016-03-09 16:23:54 -0800

    media content v1

--- app/controllers/media_content_controller.rb
+++ app/controllers/media_content_controller.rb
@@ -5,6 +5,7 @@ class MediaContentController < SecuredController
     session[:current_page] = "media_content"
     client = user_client
     path = "/Media\ Content"
+    threads = []
 
     # get parent media folder
     mediaFolder = Rails.cache.fetch("/folder/#{session[:box_id]}/media_folder", :expires_in => 10.minutes) do
@@ -16,48 +17,77 @@ class MediaContentController < SecuredController
       client.folder_items(mediaFolder, fields: [:id, :name])
     end
 
-    # get all networks folders
+
+    # NBC Network
     @nbc = @networks.find {|s| s.name == 'NBC' }
     @nbcItems = Rails.cache.fetch("/folder/#{session[:box_id]}/nbc_items", :expires_in => 10.minutes) do
-      client.folder_items(@nbc, fields: [:id, :name, :description])
+      client.folder_items(@nbc, fields: [:id, :name, :description, :created_at])
     end
 
     @nbcItems.each do |n|
-      class << n
-        attr_accessor :imageId, :videoId
-      end
-      showFolder = client.folder_items(n, fields: [:id, :name])
-      imageFile = showFolder.find {|s| s.name.split('.').last == 'jpg' }
-      trailerFile = showFolder.find {|s| s.name.split('.').last == 'mp4' }
+      threads << Thread.new do
+        class << n
+          attr_accessor :imageId, :videoId, :videoDescription, :videoName,
+                        :rating, :staring
+        end
 
-      n.imageId = imageFile.id
-      n.videoId = trailerFile.id
+        # get network folder items
+        showFolder = Rails.cache.fetch("/folder/#{session[:box_id]}/nbc_items/#{n.id}", :expires_in => 10.minutes) do
+          client.folder_items(n, fields: [:id, :name, :description])
+        end
+        imageFile = showFolder.find {|s| s.name.split('.').last == 'jpg' }
+        trailerFile = showFolder.find {|s| s.name.split('.').last == 'mp4' }
+
+        # get video file metadata
+        meta = Rails.cache.fetch("/folder/#{session[:box_id]}/nbc_items/#{n.id}_meta", :expires_in => 10.minutes) do
+          client.all_metadata(trailerFile)["entries"].first
+        end
+
+        # add file attributes
+        n.imageId = imageFile.id
+        n.videoId = trailerFile.id
+        n.videoDescription = trailerFile.description
+        n.videoName = trailerFile.name
+        n.staring = meta["Related Names"]
+        n.rating = meta["Content Rating"]
+      end
+
     end
 
 
-    @usa = @networks.find {|s| s.name == 'USA Networks' }
+    # USA Network
+    @usa = @networks.find {|s| s.name == 'USA Network' }
     @usaItems = Rails.cache.fetch("/folder/#{session[:box_id]}/usa_items", :expires_in => 10.minutes) do
-      client.folder_items(@usa, fields: [:id, :name, :description])
+      client.folder_items(@usa, fields: [:id, :name, :description, :created_at])
     end
     @usaItems.each do |n|
-      class << n
-        attr_accessor :imageId, :videoId
-      end
-      showFolder = client.folder_items(n, fields: [:id, :name])
-      imageFile = showFolder.find {|s| s.name.split('.').last == 'jpg' }
-      trailerFile = showFolder.find {|s| s.name.split('.').last == 'mp4' }
+      threads << Thread.new do
+        class << n
+          attr_accessor :imageId, :videoId, :videoDescription, :videoName,
+                        :rating, :staring
+        end
+        # get network folder items
+        showFolder = Rails.cache.fetch("/folder/#{session[:box_id]}/usa_items/#{n.id}", :expires_in => 10.minutes) do
+          client.folder_items(n, fields: [:id, :name, :description])
+        end
+        imageFile = showFolder.find {|s| s.name.split('.').last == 'jpg' }
+        trailerFile = showFolder.find {|s| s.name.split('.').last == 'mp4' }
 
-      n.imageId = imageFile.id
-      n.videoId = trailerFile.id
+        # get video file metadata
+        meta = Rails.cache.fetch("/folder/#{session[:box_id]}/usa_items/#{n.id}_meta", :expires_in => 10.minutes) do
+          client.all_metadata(trailerFile)["entries"].first
+        end
+
+        # add file attributes
+        n.imageId = imageFile.id
+        n.videoId = trailerFile.id
+        n.videoDescription = trailerFile.description
+        n.videoName = trailerFile.name
+        n.staring = meta["Related Names"]
+        n.rating = meta["Content Rating"]
+      end
     end
 
-
-
-    @cnbc = @networks.find {|s| s.name == 'CNBC' }
-    @e = @networks.find {|s| s.name == 'E!' }
-
-
-    # PROMO!!!!
-
+    threads.each { |thr| thr.join }
   end
 end
