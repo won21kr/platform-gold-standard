db17d8fafd35a177c78b5c9fde1db5acc95dc17f
Platform <mattmitchell650@gmail.com>
2016-04-05 10:50:37 -0700

    fixing active record issues

--- db/migrate/20160328224008_add_okta_to_userconfigs.rb
+++ db/migrate/20160328224008_add_okta_to_userconfigs.rb
@@ -1,5 +1,5 @@
-# class AddOktaToUserconfigs < ActiveRecord::Migration
-#   def change
-#     add_column :userconfigs, :okta, :boolean
-#   end
-# end
+class AddOktaToUserconfigs < ActiveRecord::Migration
+  def change
+    add_column :userconfigs, :okta, :boolean
+  end
+end
