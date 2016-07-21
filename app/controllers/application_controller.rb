449254b7c1c992b6dce9d4d932908d3b2863ca4b
mattmitchell6 <mattmitchell650@gmail.com>
2016-07-21 10:54:12 -0700

    updated mixpanel to include people

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,10 +42,14 @@ class ApplicationController < ActionController::Base
 
   # Capture tab event
   def mixpanel_tab_event(tab, event)
+
+    event = {}
+    event[:tab] = tab
+    event[:event] = event
+    event[:company] = session[:company] unless session[:company].blank?
+
     tracker = Mixpanel.client
-    event = tracker.track('1234', 'Tab Usage', {:tab => tab,
-                                                :event => event,
-                                                :user => session[:userinfo]['info']['name']})
+    event = tracker.track(session[:box_id], 'Tab Usage', event)
   end
 
   private
